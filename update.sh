6cea5d0e524d7b82656e6074b75c34a34677a325
Matej Sc <47155638+matesc4@users.noreply.github.com>
2019-11-24 11:54:44 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/zsh
+
+## Ubuntu update file with zsh and zprezto-update
+## source ~/.update.sh
+
+myreturn (){
+if [[ $1 != "0" ]]; then
+        exit $1
+fi
+}
+
+echo
+sudo snap refresh
+myreturn $?
+
+echo
+sudo apt update --fix-missing
+sudo apt-get update --fix-missing
+myreturn $?
+
+echo
+sudo apt full-upgrade -y --fix-missing
+sudo apt-get full-upgrade -y --fix-missing
+myreturn $?
+
+echo
+sudo apt dist-upgrade -y --fix-missing
+sudo apt-get dist-upgrade -y --fix-missing
+myreturn $?
+
+echo
+sudo apt upgrade -y --fix-missing
+sudo apt-get upgrade -y --fix-missing
+myreturn $?
+
+echo
+sudo apt autoremove -y
+sudo apt-get autoremove -y
+myreturn $?
+
+echo
+zprezto-update
+myreturn $?
+
+echo
+echo FINISH
+return 0
