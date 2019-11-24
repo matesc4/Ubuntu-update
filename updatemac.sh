a4bc850576c667fa74c063a3c47120fc3f6bf0ec
Matej Sc <47155638+matesc4@users.noreply.github.com>
2019-11-24 11:59:23 +0100

    Create updatemac.sh

---
+++ updatemac.sh
@@ -0,0 +1,51 @@
+#!/usr/local/bin/zsh
+
+## updates mac with brew / cask and zpresto-update
+## source ~/updateall.sh || echo $?
+
+
+myreturn (){
+if [[ $1 != "0" ]]; then
+        exit $1
+fi
+}
+
+myperform3 (){
+echo
+echo brew $1 $2 $3
+echo
+brew $1 $2 $3
+myreturn $?
+sleep 1
+}
+
+myperform2 (){
+echo
+echo brew $1 $2
+echo
+brew $1 $2
+myreturn $?
+sleep 1
+}
+
+myperform3 cleanup -s --verbose
+#echo gem cleanup --verbose
+myperform2 config --verbose
+brew doctor --verbose ## currently without return check, bc. of an brew error notification
+myperform3 update --force --verbose
+myperform2 upgrade --verbose
+myperform3 cask audit --verbose
+myperform3 cask doctor --verbose
+#myperform3 cask style --fix
+myperform3 cask upgrade --verbose
+#myperform2 cleanup --verbose
+
+sleep 1
+echo
+echo zprezto-update
+zprezto-update
+myreturn $?
+
+echo
+echo FINISH
+return 0
